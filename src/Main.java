0a542ef68548764abbdc22f53a2825882227442b
user <gulira.abdukerimkyzy.jk@gmail.com>
2023-01-28 21:20:16 +0600

    varargs

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(contains(15, 34, 5, 6, 7, 1, 3,15,33,56));
+    }
+    /*Кайтаруучу тиби boolean болгон метод түзүңүз,
+     параметрине бир сан жана сандардын varargs-ын кабыл алсын.
+    Методко биринчи берген аргументиңизди
+     экинчи берген аргументтердин ичинен барбы же жокпу текшерип,
+    бар болсо true, жок болсо false кайтарсын.*/
+
+    static boolean contains (int a, int ... varargs) {
+        for (int i = 0; i < varargs.length ; i++) {
+            if (varargs[i] == a) {
+                return true;
+            }
+        }
+        return false;
     }
 }
